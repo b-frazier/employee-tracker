0a09ad591cb46a44fc1277227abb60265bbab272
bfrazier <bthehippie2@gmail.com>
2023-01-28 16:33:39 -0500

    connect database

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "company_db",
+  },
+  console.log(`connected to the database: company_db`)
+);
