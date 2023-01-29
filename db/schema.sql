b6437e4924e031d8a599e76d301cd3fce848b98c
bfrazier <bthehippie2@gmail.com>
2023-01-29 17:06:43 -0500

    add auto increment

--- db/schema.sql
+++ db/schema.sql
@@ -4,22 +4,23 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30)
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) 
-        REFERENCES department(id)
-)
+        REFERENCES department(id),
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
@@ -28,4 +29,4 @@ CREATE TABLE employee (
         REFERENCES role(id),
     FOREIGN KEY (manager_id)
         REFERENCES employee(id)
-)
+);
