562b1a055910c441e3007a658d39576cfa3dea78
bfrazier <bthehippie2@gmail.com>
2023-01-29 17:35:22 -0500

    seed employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,6 +15,15 @@ INSERT INTO role(department_id, title, salary)
             (3, 'Legal Director', 250000),
             (3, 'Lawyer', 200000),
             (4, 'Accounting Director', 120000),
-            (4, 'Accountant', 100000);
+            (4, 'Accountant', 100000); 
 
-INSERT INTO employee()
+INSERT INTO employee(manager_id, first_name, last_name, role_id)
+    VALUES  (null, 'Teresa', 'Hensley', 1),
+            (1, 'Everly', 'Farner', 2),
+            (1, 'Juan', 'Sanchez', 3),
+            (null, 'Tariq', 'Moore', 4),
+            (4, 'Charlie', 'Hanna', 5),
+            (null, 'Safiya', 'Todd', 6),
+            (6, 'Jaden', 'Doyle', 7),
+            (null, 'Sabrina', 'Stuart', 8),
+            (8, 'Brodie', 'Riggs', 9);
