eb33c746212b9f9ab055c527c0a8cea8350c3614
bfrazier <bthehippie2@gmail.com>
2023-01-29 19:29:28 -0500

    write start prompt

--- server.js
+++ server.js
@@ -11,3 +11,22 @@ const db = mysql.createConnection(
   },
   console.log(`connected to the database: company_db`)
 );
+
+function start() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+      name: "action",
+    },
+  ]);
+}
