a230fe27f23f10da2738193c24e6bf39581d3940
bfrazier <bthehippie2@gmail.com>
2023-02-02 22:06:24 -0500

    properly logged table to console

--- server.js
+++ server.js
@@ -1,61 +1,60 @@
-const mysql = require("mysql2");
-const inquirer = require("inquirer");
-require("dotenv").config();
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('dotenv').config();
 
 const db = mysql.createConnection(
   {
-    host: "localhost",
+    host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
-  console.log(`connected to the database: company_db`),
-  start()
+  console.log(`connected to the database: company_db`)
 );
 
 function start() {
   inquirer
     .prompt([
       {
-        type: "list",
-        message: "What would you like to do?",
+        type: 'list',
+        message: 'What would you like to do?',
         choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
-          "Nothing more to do",
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Nothing more to do',
         ],
-        name: "action",
+        name: 'action',
       },
     ])
     .then(function (input) {
       switch (input.action) {
-        case "View all departments":
-          viewAll("departments");
+        case 'View all departments':
+          viewAll('department');
           break;
-        case "View all roles":
-          viewAll();
+        case 'View all roles':
+          viewAll('roles');
           break;
-        case "View all employees":
-          viewAll();
+        case 'View all employees':
+          viewAll('employees');
           break;
-        case "Add a department":
+        case 'Add a department':
           addOption();
           break;
-        case "Add a role":
+        case 'Add a role':
           addOption();
           break;
-        case "Add an employee":
+        case 'Add an employee':
           addOption();
           break;
-        case "Update an employee role":
+        case 'Update an employee role':
           addOption();
           break;
-        case "Nothing more to do":
+        case 'Nothing more to do':
           stop();
           break;
       }
@@ -63,8 +62,22 @@ function start() {
 }
 
 function viewAll(option) {
-  if (option === "departments") {
-    console.log("show me the departments");
+  if (option === 'department') {
+    db.query(`SELECT * FROM department`, function (err, res) {
+      console.table(res);
+      if (err) console.log(err);
+    });
+  } else if (option === 'roles') {
+    db.query(`SELECT * FROM role`, function (err, res) {
+      console.table(res);
+      if (err) console.log(err);
+    });
+  } else if (option === 'employees') {
+    db.query(`SELECT * FROM employee`, function (err, res) {
+      console.table(res);
+      if (err) console.log(err);
+    });
   }
-  start();
 }
+
+start();
