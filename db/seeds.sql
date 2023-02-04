8a73cbc8ec98f136deb5abbbc07bbb1ff9ada9a4
bfrazier <bthehippie2@gmail.com>
2023-02-04 13:27:20 -0500

    fix sql syntax

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,15 +2,15 @@ USE company_db;
 
 INSERT INTO department(name)
     VALUES  ('Executive'),
-    VALUES  ('Engineering'),
+            ('Engineering'),
             ('Sales'),
             ('Legal'),
             ('Finance');
 
 INSERT INTO role(department_id, title, salary)
     VALUES  (1, 'Owner', 600000),
-    VALUES  (1, 'Partner', 450000),
-    VALUES  (2, 'Lead Engineer', 150000),
+            (1, 'Partner', 450000),
+            (2, 'Lead Engineer', 150000),
             (2, 'Software Engineer', 130000),
             (2, 'Software Engineer Intern', 50000),
             (3, 'Sales Director', 135000),
@@ -22,7 +22,7 @@ INSERT INTO role(department_id, title, salary)
 
 INSERT INTO employee(manager_id, first_name, last_name, role_id)
     VALUES  (null, 'Bianca', 'Frazier', 1),
-    VALUES  (1, 'Teresa', 'Hensley', 3),
+            (1, 'Teresa', 'Hensley', 3),
             (2, 'Everly', 'Farner', 4),
             (2, 'Juan', 'Sanchez', 5),
             (1, 'Tariq', 'Moore', 6),
