2443656497e7b075d8de0832f91599edc17bd1ae
bfrazier <bthehippie2@gmail.com>
2023-02-04 13:12:58 -0500

    added roles for easier user experience

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,33 @@
 USE company_db;
 
 INSERT INTO department(name)
+    VALUES  ('Executive'),
     VALUES  ('Engineering'),
             ('Sales'),
             ('Legal'),
             ('Finance');
 
 INSERT INTO role(department_id, title, salary)
-    VALUES  (1, 'Lead Engineer', 150000),
-            (1, 'Software Engineer', 130000),
-            (1, 'Software Engineer Intern', 50000),
-            (2, 'Sales Director', 135000),
-            (2, 'Salesperson', 80000),
-            (3, 'Legal Director', 250000),
-            (3, 'Lawyer', 200000),
-            (4, 'Accounting Director', 120000),
-            (4, 'Accountant', 100000); 
+    VALUES  (1, 'Owner', 600000),
+    VALUES  (1, 'Partner', 450000),
+    VALUES  (2, 'Lead Engineer', 150000),
+            (2, 'Software Engineer', 130000),
+            (2, 'Software Engineer Intern', 50000),
+            (3, 'Sales Director', 135000),
+            (3, 'Salesperson', 80000),
+            (4, 'Legal Director', 250000),
+            (4, 'Lawyer', 200000),
+            (5, 'Accounting Director', 120000),
+            (5, 'Accountant', 100000); 
 
 INSERT INTO employee(manager_id, first_name, last_name, role_id)
-    VALUES  (null, 'Teresa', 'Hensley', 1),
-            (1, 'Everly', 'Farner', 2),
-            (1, 'Juan', 'Sanchez', 3),
-            (null, 'Tariq', 'Moore', 4),
-            (4, 'Charlie', 'Hanna', 5),
-            (null, 'Safiya', 'Todd', 6),
-            (6, 'Jaden', 'Doyle', 7),
-            (null, 'Sabrina', 'Stuart', 8),
-            (8, 'Brodie', 'Riggs', 9);
+    VALUES  (null, 'Bianca', 'Frazier', 1),
+    VALUES  (1, 'Teresa', 'Hensley', 3),
+            (2, 'Everly', 'Farner', 4),
+            (2, 'Juan', 'Sanchez', 5),
+            (1, 'Tariq', 'Moore', 6),
+            (5, 'Charlie', 'Hanna', 7),
+            (1, 'Safiya', 'Todd', 8),
+            (7, 'Jaden', 'Doyle', 9),
+            (1, 'Sabrina', 'Stuart', 10),
+            (9, 'Brodie', 'Riggs', 11);
